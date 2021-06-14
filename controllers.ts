bb6454bab994c8490692663041db350ac12af3b5
Nehuen15 <nehuencarrizo@outlook.com.ar>
2021-06-14 14:23:08 -0300

    Async-promises

--- controllers.ts
+++ controllers.ts
@@ -1,27 +1,29 @@
 import { ContactsCollection, Contact } from "./models";
 
 export class ContactsControllerOptions {
-  action: "get" | "save";
-  params: Contact;
+    action: "get" | "save";
+    params: Contact;
 }
 
 class ContactsController {
-  contacts: ContactsCollection;
-  constructor() {
-    this.contacts = new ContactsCollection();
-    this.contacts.load();
-  }
-  processOptions(options: ContactsControllerOptions) {
-    var resultado;
-    if (options.action == "get" && options.params.id) {
-      resultado = this.contacts.getOneById(options.params.id);
-    } else if (options.action == "get") {
-      resultado = this.contacts.getAll();
-    } else if (options.action == "save" && options.params) {
-      this.contacts.addOne(options.params);
-      this.contacts.save();
+    contacts: ContactsCollection;
+    promesa: Promise<any>;
+    constructor() {
+        this.contacts = new ContactsCollection();
+        const promesa = this.contacts.load();
+        this.promesa = promesa;
     }
-    return resultado;
-  }
+    processOptions(options: ContactsControllerOptions) {
+        var resultado;
+        if (options.action == "get" && options.params.id) {
+            resultado = this.contacts.getOneById(options.params.id);
+        } else if (options.action == "get") {
+            resultado = this.contacts.getAll();
+        } else if (options.action == "save" && options.params) {
+            this.contacts.addOne(options.params);
+            this.contacts.save();
+        }
+        return resultado;
+    }
 }
 export { ContactsController };
--- models.ts
+++ models.ts
@@ -1,35 +1,38 @@
 import * as jsonfile from "jsonfile";
 
 class Contact {
-  id: number;
-  name: string;
+    id: number;
+    name: string;
 }
 
 class ContactsCollection {
-  data: Contact[] = [];
-  load() {
-    // usar la version Async (readFile)
-    const json = jsonfile.readFileSync("./contacts.json");
-    this.data = json;
-  }
-  getAll() {
-    return this.data;
-  }
-  addOne(contact: Contact) {
-    this.data.push(contact);
-  }
-  save() {
-    // usar la version Async (writeFIle)
-    jsonfile.writeFileSync("./contacts.json", this.data);
-  }
-  getOneById(id) {
-    const encontrado = this.data.find((contacto) => {
-      if (contacto.id == id) {
-        return true;
-      }
-    });
+    data: Contact[] = [];
+    load(): Promise<any> {
+        // usar la version Async (readFile)
+        const promise: Promise<any> = jsonfile.readFile("./contacts.json");
+        promise.then((obj) => {
+            this.data = obj;
+        });
+        return promise;
+    }
+    getAll() {
+        return this.data;
+    }
+    addOne(contact: Contact) {
+        this.data.push(contact);
+    }
+    save() {
+        // usar la version Async (writeFIle)
+        jsonfile.writeFile("./contacts.json", this.data);
+    }
+    getOneById(id) {
+        const encontrado = this.data.find((contacto) => {
+            if (contacto.id == id) {
+                return true;
+            }
+        });
 
-    return encontrado;
-  }
+        return encontrado;
+    }
 }
 export { ContactsCollection, Contact };
