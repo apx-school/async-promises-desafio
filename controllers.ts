4c2535f74a06f6146466f25a072f9f4bc2c04280
Your Name <you@example.com>
2021-11-01 18:05:05 -0300

    disque anda

--- controllers.ts
+++ controllers.ts
@@ -1,23 +1,25 @@
-import { ContactsCollection, Contact } from "./models";
+import { ContactsCollection, Contact } from './models';
 
 export class ContactsControllerOptions {
-  action: "get" | "save";
+  action: 'get' | 'save';
   params: Contact;
 }
 
 class ContactsController {
   contacts: ContactsCollection;
+  laod: Promise<any>;
   constructor() {
     this.contacts = new ContactsCollection();
-    this.contacts.load();
+    const promise = this.contacts.load();
+    this.laod = promise;
   }
   processOptions(options: ContactsControllerOptions) {
     var resultado;
-    if (options.action == "get" && options.params.id) {
+    if (options.action == 'get' && options.params.id) {
       resultado = this.contacts.getOneById(options.params.id);
-    } else if (options.action == "get") {
+    } else if (options.action == 'get') {
       resultado = this.contacts.getAll();
-    } else if (options.action == "save" && options.params) {
+    } else if (options.action == 'save' && options.params) {
       this.contacts.addOne(options.params);
       this.contacts.save();
     }
