50e4848e86a5b42024217db13f214da9c736dc96
Matias Ferreira <ferre.matias@hotmail.com>
2022-10-06 19:39:57 -0300

    finalizado

--- src/controllers.ts
+++ src/controllers.ts
@@ -7,21 +7,24 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
+  contactosLeidos: Promise<any>;
   constructor() {
     this.contacts = new ContactsCollection();
-    this.contacts.load();
+    this.contactosLeidos = this.contacts.load();
   }
-  processOptions(options: ContactsControllerOptions) {
-    var resultado;
-    if (options.action == "get" && options.params.id) {
-      resultado = this.contacts.getOneById(options.params.id);
-    } else if (options.action == "get") {
-      resultado = this.contacts.getAll();
-    } else if (options.action == "save" && options.params) {
-      this.contacts.addOne(options.params);
-      this.contacts.save();
-    }
-    return resultado;
+  processOptions(options: ContactsControllerOptions): Promise<any> {
+    return this.contactosLeidos.then(() => {
+      var resultado;
+      if (options.action == "get" && options.params.id) {
+        resultado = this.contacts.getOneById(options.params.id);
+      } else if (options.action == "get") {
+        resultado = this.contacts.getAll();
+      } else if (options.action == "save" && options.params) {
+        this.contacts.addOne(options.params);
+        resultado = this.contacts.save();
+      }
+      return resultado;
+    });
   }
 }
 export { ContactsController };
